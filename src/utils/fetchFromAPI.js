69599ea3dbfb904d237ebd29b2d3cfe6ed75ece6
Soham Hegde <sohamhegdessj4@gmail.com>
2023-08-02 11:01:24 +0530

    added env var

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,6 +1,6 @@
+
+
 import axios from "axios";
-
-
 
 let BASE_URL = 'https://youtube-v31.p.rapidapi.com'
 
@@ -9,12 +9,12 @@ const options = {
     maxResults: '50'
   },
   headers: {
-    'X-RapidAPI-Key': '24d26049a7msha1220be216e6e38p1c282cjsnac36b4cd7c76',
+    'X-RapidAPI-Key': import.meta.env.VITE_REACT_API_KEY,
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
   }
 };
-export const fetchFromAPI = async (url) =>{
-   const {data} =  await axios.get(`${BASE_URL}/${url}`,options)
-   return data
-}
 
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+}
